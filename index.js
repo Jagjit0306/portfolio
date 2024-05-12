9b64191f191f350df4f4f20290401b68968f733e
Jagjit0306 <146818104+Jagjit0306@users.noreply.github.com>
2024-05-13 02:39:42 +0530

    refinements

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ setInterval(()=>{
             pfp.classList.add('pfp-big-pers')
             pfp.classList.remove('pfp-big')
         } )
-        // pfp.classList.remove('pfp-small')
     }
     else if(window.scrollY && ontop) {
         pfp.classList.remove('pfp-big-pers')
@@ -19,27 +18,8 @@ setInterval(()=>{
             pfp.classList.remove('pfp-big-pers')
         })
         ontop = false
-        // pfp.classList.remove('pfp-big')
     }
 },100)
-
-$(document).ready(function() {
-    var cards = $('.card');
-    var currentIndex = 0;
-  
-    function showCard(index) {
-      cards.removeClass('center');
-      cards.eq(index).addClass('center');
-    }
-  
-    function cycleCards() {
-      showCard(currentIndex);
-      currentIndex = (currentIndex + 1) % cards.length;
-      setTimeout(cycleCards, 2000); // Adjust the timing here (milliseconds)
-    }
-  
-    cycleCards();
-  });  
 
 function main() {
     let bar_els = document.getElementsByClassName('bar-el')
@@ -51,15 +31,20 @@ function main() {
                     left: 0,
                     behavior: "smooth",
                   });
-            })
-        }
-        if(i==1){
-            bar_els[i].addEventListener('click', ()=>{
-                window.scrollTo({
-                    top: 395,
-                    left: 0,
-                    behavior: "smooth",
-                  });
+                })
+            }
+            if(i==1){
+                bar_els[i].addEventListener('click', ()=>{
+                    window.scrollTo({
+                        top: 395,
+                        left: 0,
+                        behavior: "smooth",
+                    });
+                    let h = document.getElementsByClassName('headings')
+                    h[1].classList.add('headings-anim')
+                    h[1].addEventListener('animationend', (e)=>{
+                        h[1].classList.remove('headings-anim')
+                    })        
             })
         }
         if(i==2){
@@ -69,6 +54,11 @@ function main() {
                     left: 0,
                     behavior: "smooth",
                   });
+                  let h = document.getElementsByClassName('headings')
+                    h[2].classList.add('headings-anim')
+                    h[2].addEventListener('animationend', (e)=>{
+                        h[2].classList.remove('headings-anim')
+                    }) 
             })
         }
     }
